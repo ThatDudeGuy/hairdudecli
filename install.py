4cd2446e4b2b534ca1fa3fbd8abb566f1df40080
ThatDudeGuy <waffles1999@icloud.com>
2026-04-24 03:51:53 -0400

    Fix install script and add uninstall and update to top-level options

--- install.py
+++ install.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 import subprocess
+import pathlib
 
 def try_run_command(command):
     try:
-        subprocess.run(command)
+        subprocess.run(command.split(','))
+        print(" ".join(command))
     except Exception as e:
-        print("Install failure...")
-        print(f"Failed to run command '{command}' | {e}")
-        raise
+        raise Exception(f"Failed to run command '{command}'") from e
+
+cwd = pathlib.Path.cwd()
 
 commands = [
-    "cp -r ./hairdudecli /usr/local/lib/",
-    "cp ./hdcli.py /usr/local/bin/hdcli",
-    "cp ./auto_complete/hdcli /usr/share/bash-completion/completions/hdcli",
+    ["cp", "-r", f"{cwd}/hairdudecli/", "/usr/local/lib/"],
+    ["cp", f"{cwd}/hdcli.py", "/usr/local/bin/hdcli"],
+    ["chmod", "+x", "/usr/local/bin/hdcli"],
+    ["cp", f"{cwd}/auto_complete/hdcli", "/usr/share/bash-completion/completions/hdcli"],
 ]
 
 for command in commands:
